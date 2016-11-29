b865b95711777f4f59c0a8306b1162fc50ecdf16
liming644472615 <liming644472615@qq.com>
2016-11-29 16:11:08 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,46 @@
+window.onload=function(){
+    $(".box").mousedown(function(e){
+        e.preventDefault();
+    });
+    $(".box").mousemove(function(e){
+        e.preventDefault();
+    });
+    var marginL;
+    var clientw=$(".box").width();
+    var num=0;
+    touch.on("body","dragstart",".banner",function(e){
+        marginL=parseInt($(".banner").css("marginLeft")?$(".banner").css("marginLeft"):0);
+        $(".banner").css({
+            transition:"none"
+        })
+    });
+    touch.on("body","drag",".banner",function(e){
+        $(".banner").css({
+            marginLeft:marginL+ e.x,
+        })
+    })
+    touch.on("body","dragend",".banner",function(e){
+        if(Math.abs(e.x)>150){
+            if(e.direction=="left"){
+                num++;
+                if(num==$(".imgs").length){
+                    num=$(".imgs").length-1;
+                }
+            }else if(e.direction=="right"){
+                num--;
+                if(num==-1){
+                    num=0;
+                }
+            }
+            $(".banner").css({
+                marginLeft:-num*clientw,
+                transiton:"margin-left 2s ease"
+            })
+        }else{
+            $(".banner").css({
+                marginLeft:num*clientw,
+                transiton:"margin-left 2s ease"
+            })
+        }
+    })
+}
